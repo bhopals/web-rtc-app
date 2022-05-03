a187b8d7977f50eec2859f4685b3796826ee2e1d
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-05-03 17:31:41 -0400

    refactor

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+// https://dev.to/yuribenjamin/how-to-deploy-react-app-in-github-pages-2a1f
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
