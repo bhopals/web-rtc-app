c7c8f26a2ae7190155bf80e4e29fe3aaa25ff4ea
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-05-04 08:15:44 -0400

    add steps to run end to end

---
+++ src/test/suite.js
@@ -0,0 +1,5 @@
+Reference:
+https://www.toptal.com/webrtc/taming-webrtc-with-peerjs
+https://blog.logrocket.com/creating-chat-application-with-webrtc/
+ws://chat-app-signaller.herokuapp.com
+https://github.com/jkithome/signalling-server
